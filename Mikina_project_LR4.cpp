7508603e03db1ad543ab044f38eb7b58e3e4d1ec
mikinaksu <mikinaksu8@gmail.com>
2025-04-15 09:20:06 +0300

    correct mistakes 2

--- Mikina_project_LR4.cpp
+++ Mikina_project_LR4.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <functional>
+
 using namespace std;
 
-function<void()> EnterNum(int& varLink, string label) 
-{
-    //разрабатывается Сидоровым С. - ветка branch_fun_1
-    return [&varLink,label]() {
-        cout << label << " = ";
-        cin >> varLink;
-    };
+// Функция для ввода числа A
+int EnterA() {
+    //разрабатывается Ивановым П. - ветка branch_fun_1
+
 }
-double EnterA() {
+
+// Функция для ввода числа B
+int EnterB() {
     //разрабатывается Ивановым П. - ветка branch_fun_2
 }
-double EnterB() {
+
+// Функция для ввода числа C
+int EnterC() {
     //разрабатывается Ивановым П. - ветка branch_fun_3
 }
-double EnterC() {
-    //разрабатывается Ивановым П. - ветка branch_fun_4
+
+// Функция для нахождения остатка от деления разности A и B на C
+int CalcOstatok(int A, int B, int C) {
+    //разрабатывается Ивановым A. - ветка branch_fun_4
 }
-double CalcOstatok(int num_a)
-{
-    //разрабатывается Петровым П. - ветка branch_fun_2
+
+// Функция для нахождения целой части от деления A на сумму B и C
+int CalcCelayaPart(int A, int B, int C) {
+    //разрабатывается Сидоровым П. - ветка branch_fun_5
 }
-double CalcCelayaPart(int num_a) 
-{
-    //разрабатывается Василенко А. - ветка branch_fun_3
+
+int main() {
+    map<string, function<int()>> menu;
+
+    // Заполнение меню функциями
+    menu["EnterA"] = EnterA;
+    menu["EnterB"] = EnterB;
+    menu["EnterC"] = EnterC;
+
+    int A = menu["EnterA"]();
+    int B = menu["EnterB"]();
+    int C = menu["EnterC"]();
+
+    // Вычисление остатка
+    int ostatok = CalcOstatok(A, B, C);
+    cout << "Остаток от деления (A - B) % C = " << ostatok << endl;
+
+    // Вычисление целой части
+    int celayaPart = CalcCelayaPart(A, B, C);
+    cout << "Целая часть от деления A / (B + C) = " << celayaPart << endl;
+
+    return 0;
 }
 
